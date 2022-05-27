b6cffc4ad46afe482df28d65a641cceddfbc594c
vdlaptrinh <vdlaptrinh@gmail.com>
2022-05-27 14:57:40 +0700

    Add sản phẩm

---
+++ LTSP10/app/src/main/java/com/vtmsoft/ltsp10/MainActivity.java
@@ -0,0 +1,162 @@
+package com.vtmsoft.ltsp10;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+
+import com.vtmsoft.ltsp10.adapter.ProductAdapter;
+import com.vtmsoft.ltsp10.models.Product;
+import com.vtmsoft.ltsp10.models.SaleManager;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ProductAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.setTitle("DANH SÁCH");
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+        //actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        lv = (ListView) findViewById(R.id.lvProducts);
+        //Khởi tạo các sản phẩm
+        SaleManager saleManager = SaleManager.get();
+        saleManager.generateProducts();
+        //lấy các product từ class saleManager
+        ArrayList products = saleManager.getProducts();
+        adapter = new ProductAdapter(this, products);//khởi tạo adapter
+        lv.setAdapter(adapter);//hiển lên listview
+        //long click để xoá
+        lv.setOnItemLongClickListener(new ItemLongClickRemove());
+        //click để sửa
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Intent intent = new Intent(MainActivity.this, ProductActivity.class);
+                intent.putExtra(ProductActivity.EXTRA_POSITION, position);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btnAdd:
+                //Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                //Them san pham
+                openMyCustom();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class ItemLongClickRemove implements AdapterView.OnItemLongClickListener {
+        @Override
+        public boolean onItemLongClick(AdapterView parent, View view, final int position, long id) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+            alertDialogBuilder.setMessage("Bán có muốn xóa sản phẩm này!");
+            alertDialogBuilder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // xóa sp đang nhấn giữ
+                    SaleManager.get().getProducts().remove(position);
+                    //cập nhật lại listview
+                    adapter.notifyDataSetChanged();
+
+                }
+            });
+            alertDialogBuilder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //không làm gì
+                }
+            });
+            alertDialogBuilder.show();
+            return true;
+        }
+    }
+
+    public void openMyCustom() {
+
+        // khởi tạo AlertDialog từ đối tượng Builder. tham số truyền vào ở đây là context.
+        //final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog builder = new AlertDialog.Builder(this).create();
+
+        // khoi tao doi tuong View tu file mydialog.
+        final View alert = LayoutInflater.from(this).inflate(R.layout.add_item, null);
+
+        // set layout cho alert dialog
+        builder.setView(alert);
+
+        final EditText txtProductName;
+        final EditText txtUnit;
+        final EditText txtPrice;
+        Button btnThem;
+        Button btnHuy;
+
+        // tham chieu cac doi tuong co tren giao dien dialog vua duoc set
+        txtProductName = alert.findViewById(R.id.txtProductName);
+        txtUnit = alert.findViewById(R.id.txtUnit);
+        txtPrice = alert.findViewById(R.id.txtPrice);
+        btnThem = alert.findViewById(R.id.btnThem);
+        btnHuy = alert.findViewById(R.id.btnHuy);
+
+        // tạo dialog và hiển thị
+        builder.show();
+
+        // bat su kien click vao nut
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Product product = new Product();
+                product.setProductName(txtProductName.getText().toString());
+                product.setUnit(txtUnit.getText().toString());
+                String s = txtPrice.getText().toString();
+                s = s.replace(",", "");
+                double price = Double.parseDouble(s);
+                product.setPrice(price);
+                adapter.add(product);
+                adapter.notifyDataSetChanged();
+                builder.dismiss();
+            }
+        });
+
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.dismiss();
+            }
+        });
+
+    }
+
+}
---
+++ LTSP10/app/src/main/java/com/vtmsoft/ltsp10/ProductActivity.java
@@ -0,0 +1,92 @@
+package com.vtmsoft.ltsp10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.vtmsoft.ltsp10.adapter.ProductAdapter;
+import com.vtmsoft.ltsp10.models.Product;
+import com.vtmsoft.ltsp10.models.SaleManager;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class ProductActivity extends Activity {
+    public final static String EXTRA_POSITION = "position";
+    EditText txtProductName,txtUnit,txtPrice;
+    Product product;
+    int position;
+    Button btnOK,btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+        //lấy các control trên layout
+        txtProductName = (EditText)findViewById(R.id.txtProductName);
+        txtUnit = (EditText)findViewById(R.id.txtUnit);
+        txtPrice = (EditText)findViewById(R.id.txtPrice);
+
+        btnOK = (Button)findViewById(R.id.btnOK);
+        btnCancel = (Button)findViewById(R.id.btnCancel);
+        //hiển thị lên màn hình
+        Intent it = getIntent();
+        position = it.getExtras().getInt(EXTRA_POSITION);
+
+        if(position != -1) {
+            product = (Product) SaleManager.get().getProducts().get(position);
+            txtProductName.setText(product.getProductName());
+            txtUnit.setText(product.getUnit());
+            String s = (new DecimalFormat("#,###.##")).format(product.getPrice());
+            txtPrice.setText(s);
+        }
+        //position = 100;
+        //Khởi tạo các sản phẩm
+        //SaleManager saleManager = SaleManager.get();
+        //saleManager.generateProducts();
+        //lấy các product từ class saleManager
+        //Products.add(new Product("Bánh mì gừng", "Ổ", 10000));
+        //ArrayList products = saleManager.getProducts();
+        if(position == -1) {
+            product = new Product();
+            //int length = Products.lastIndexOf(product);
+            //product = (Product) SaleManager.get().getProducts().get(-1);
+            Log.d("position", "-1");
+        }
+        //thiết đặt sự kiện khi click vào các button
+        btnOK.setOnClickListener(new OKClickListener());
+        btnCancel.setOnClickListener(new CancelClickListener());
+    }
+    class OKClickListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            //lấy dữ liệu từ layout để cập nhật lại các sản phẩm trong mảng
+            product.setProductName(txtProductName.getText().toString());
+            product.setUnit(txtUnit.getText().toString());
+            String s = txtPrice.getText().toString();
+            s = s.replace(",", "");
+            double price = Double.parseDouble(s);
+            product.setPrice(price);
+            Intent returnIntent = new Intent();
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        }
+    }
+    class CancelClickListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            // không làm gì cả và trở về màng hình trước
+            Intent returnIntent = new Intent();
+            setResult(Activity.RESULT_CANCELED, returnIntent);
+            finish();
+        }
+    }
+}
